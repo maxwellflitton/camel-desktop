da6c137a310c61b7ec6ab152f4a8d5be445e4b19
Maxwell Flitton <maxwellflitton@Maxwells-MacBook-Pro.local>
2022-04-03 17:56:35 +0100

    basic builds working

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # camel-desktop
 Basic react electron react application for Oasis camel
+
+
+https://medium.com/@m.mallweger/how-to-setup-electron-with-react-and-package-it-for-distribution-976939af199e
---
+++ react_app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This app was coded by Maxwell Flitton using electron and react
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
